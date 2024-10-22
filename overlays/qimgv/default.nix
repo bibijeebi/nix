f2f11dab1c18bfbd7e8fae6b98f7dedcea6372fb
bibijeebi <bennyforeman1@gmail.com>
2024-10-22 14:23:47 -0400

    changes

--- overlays/qimgv/default.nix
+++ overlays/qimgv/default.nix
@@ -6,14 +6,14 @@
 }: final: prev: {
   qimgv = prev.qimgv.overrideAttrs (oldAttrs: {
     version = "1.0.3-alpha-128-gaeccf866";
-    src = fetchFromGitHub {
+    src = prev.fetchFromGitHub {
       owner = "easymodo";
       repo = "qimgv";
       rev = "aeccf866";
       sha256 = "sha256-24bsghJ74/w8t9MJj6R5IcexESFkARmMq6PBsm0er0Q=";
     };
-    buildInputs = oldAttrs.buildInputs ++ [libGL libGLU];
-    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [qt5.qttools];
+    buildInputs = oldAttrs.buildInputs ++ [prev.libGL prev.libGLU];
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [prev.qt5.qttools];
     cmakeFlags = oldAttrs.cmakeFlags ++ ["-DUSE_OPENGL=ON" "-DOPENCV_SUPPORT=ON"];
   });
 }
