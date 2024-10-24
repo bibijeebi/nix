fb844a1bd19c8afca51e5ed9c218646372f2aaa1
bibijeebi <bennyforeman1@gmail.com>
2024-10-24 18:39:14 -0400

    feat(x86_64-linux): update nixxie system configuration

--- systems/x86_64-linux/nixxie/default.nix
+++ systems/x86_64-linux/nixxie/default.nix
@@ -43,6 +43,34 @@ in {
     networkmanager.enable = true;
   };
 
+  stylix = {
+    enable = true;
+    image = pkgs.fetchurl {
+      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+    };
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      monospace = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans Mono";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+  };
+
   # Internationalization
   i18n = {
     defaultLocale = locale;
