662feb96cb4a76c56caff0a215f6dca2b4b6c1d0
bibijeebi <bennyforeman1@gmail.com>
2024-10-23 22:16:54 -0400

    changes

--- packages/autodesk-fusion360/default.nix
+++ packages/autodesk-fusion360/default.nix
@@ -17,9 +17,8 @@
 }:
 mkWindowsApp rec {
   pname = "fusion360";
-  version = "latest"; # Fusion 360 uses rolling releases
+  version = "latest";
 
-  # The installer is downloaded dynamically by the script, we'll need to fetch it
   src = fetchurl {
     url = "https://dl.appstreaming.autodesk.com/production/installers/Fusion%20Admin%20Install.exe";
     sha256 = "sha256:1qcw1fbl2s9p8gwa0w6df9sscxnpkip3712sjd3qz6lv625xyb46";
@@ -35,7 +34,7 @@ mkWindowsApp rec {
     copyDesktopIcons
     winetricks
     cabextract
-    wine # Needed for winetricks
+    wine
     makeWrapper
   ];
 
@@ -44,32 +43,76 @@ mkWindowsApp rec {
     libnotify
   ];
 
-  # Use wine64 explicitly
   wine = wine64;
   wineArch = "win64";
 
   dontUnpack = true;
 
-  # Create launcher script
+  # Create launcher script with proper environment setup
   launcher = writeShellScript "fusion360-launcher" ''
-    export WINEPREFIX="$HOME/.local/share/fusion360/prefix"
     export WINEARCH=win64
-    exec ${wine64}/bin/wine64 "$WINEPREFIX/drive_c/Program Files/Autodesk/webdeploy/production/"*.exe "$@"
+    export WINEPREFIX="$HOME/.local/share/fusion360/prefix"
+    export WINEDLLOVERRIDES="winemenubuilder.exe=d;mscoree=d"
+    export DXVK_LOG_LEVEL=none
+    export DXVK_HUD=0
+
+    # Create WINEPREFIX if it doesn't exist
+    if [ ! -d "$WINEPREFIX" ]; then
+      echo "Creating new Wine prefix..."
+
+      # Initialize prefix
+      ${wine64}/bin/wineboot --init
+
+      # Wait for wineboot to finish
+      while pgrep wineboot >/dev/null; do
+        sleep 1
+      done
+
+      # Install basic requirements
+      ${winetricks}/bin/winetricks -q dotnet452
+      ${winetricks}/bin/winetricks -q win10
+      ${winetricks}/bin/winetricks -q msxml4 msxml6
+      ${winetricks}/bin/winetricks -q vcrun2017
+      ${winetricks}/bin/winetricks -q atmlib gdiplus
+
+      # Copy installers
+      mkdir -p "$WINEPREFIX/drive_c/users/$USER/Downloads"
+      cp ${webview2Installer} "$WINEPREFIX/drive_c/users/$USER/Downloads/WebView2installer.exe"
+      cp ${src} "$WINEPREFIX/drive_c/users/$USER/Downloads/Fusion360installer.exe"
+
+      # Install WebView2
+      ${wine64}/bin/wine64 "$WINEPREFIX/drive_c/users/$USER/Downloads/WebView2installer.exe" /silent /install
+
+      # Install Fusion 360
+      cd "$WINEPREFIX/drive_c/users/$USER/Downloads"
+      ${wine64}/bin/wine64 Fusion360installer.exe --quiet
+
+      # Configure DLL overrides
+      ${wine64}/bin/wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "adpclientservice.exe" /t REG_SZ /d "" /f
+      ${wine64}/bin/wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "AdCefWebBrowser.exe" /t REG_SZ /d "builtin" /f
+      ${wine64}/bin/wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "msvcp140" /t REG_SZ /d "native" /f
+      ${wine64}/bin/wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "mfc140u" /t REG_SZ /d "native" /f
+      ${wine64}/bin/wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "bcp47langs" /t REG_SZ /d "" /f
+    fi
+
+    # Find and launch Fusion 360
+    FUSION_EXE=$(find "$WINEPREFIX/drive_c/Program Files/Autodesk/webdeploy/production/" -name "*.exe" -type f | head -1)
+    if [ -n "$FUSION_EXE" ]; then
+      exec ${wine64}/bin/wine64 "$FUSION_EXE" "$@"
+    else
+      echo "Error: Fusion 360 executable not found"
+      exit 1
+    fi
   '';
 
   installPhase = ''
     runHook preInstall
 
-    # Create directory structure
     mkdir -p $out/bin
     mkdir -p $out/share/applications
 
-    # Install launcher script
-    install -Dm755 ${launcher} $out/bin/.launcher
-    makeWrapper $out/bin/.launcher $out/bin/${pname} \
-      --prefix PATH : ${lib.makeBinPath [wine64]} \
-      --set WINEPREFIX "$HOME/.local/share/fusion360/prefix" \
-      --set WINEARCH "win64"
+    # Install launcher
+    install -Dm755 ${launcher} $out/bin/${pname}
 
     # Copy desktop files and icons
     copyDesktopItems
@@ -78,63 +121,12 @@ mkWindowsApp rec {
     runHook postInstall
   '';
 
-  # Initialize wine prefix before installing
-  preWineInit = ''
-    # Ensure clean prefix
-    rm -rf "$WINEPREFIX"
-    mkdir -p "$WINEPREFIX"
-  '';
-
-  # Main installation steps
-  winAppInstall = ''
-    # Initialize wine prefix with required Windows version
-    ${wine64}/bin/wineboot --init
-
-    # Wait for wineboot
-    while pgrep wineboot >/dev/null; do
-      echo "Waiting for wineboot..."
-      sleep 1
-    done
-
-    # Create required directories
-    mkdir -p "$WINEPREFIX/drive_c/users/$USER/Downloads"
-    mkdir -p "$WINEPREFIX/drive_c/users/$USER/AppData/Local/Autodesk/Neutron Platform/Options"
-    mkdir -p "$WINEPREFIX/drive_c/users/$USER/AppData/Roaming/Autodesk/Neutron Platform/Options"
-
-    # Basic Windows requirements
-    ${winetricks}/bin/winetricks -q atmlib gdiplus arial corefonts dotnet452
-    ${winetricks}/bin/winetricks -q msxml4 msxml6 vcrun2017
-    ${winetricks}/bin/winetricks -q fontsmooth=rgb winhttp win10
-
-    # Copy installers
-    cp ${src} "$WINEPREFIX/drive_c/users/$USER/Downloads/Fusion360installer.exe"
-    cp ${webview2Installer} "$WINEPREFIX/drive_c/users/$USER/Downloads/WebView2installer.exe"
-
-    # Install WebView2 first
-    ${wine64}/bin/wine64 "$WINEPREFIX/drive_c/users/$USER/Downloads/WebView2installer.exe" /install
-
-    # Install Fusion 360
-    ${wine64}/bin/wine64 "$WINEPREFIX/drive_c/users/$USER/Downloads/Fusion360installer.exe" --quiet
-
-    # Registry modifications for better compatibility
-    ${wine64}/bin/wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "adpclientservice.exe" /t REG_SZ /d "" /f
-    ${wine64}/bin/wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "AdCefWebBrowser.exe" /t REG_SZ /d "builtin" /f
-    ${wine64}/bin/wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "msvcp140" /t REG_SZ /d "native" /f
-    ${wine64}/bin/wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "mfc140u" /t REG_SZ /d "native" /f
-    ${wine64}/bin/wine reg add "HKCU\\Software\\Wine\\DllOverrides" /v "bcp47langs" /t REG_SZ /d "" /f
-  '';
-
-  # File mappings for configuration persistence
   fileMap = {
     "$HOME/.config/fusion360/SumatraPDF-settings.txt" = "drive_c/fusion360/SumatraPDF-settings.txt";
     "$HOME/.cache/fusion360" = "drive_c/fusion360/cache";
     "$HOME/.config/fusion360/config" = "drive_c/users/$USER/AppData/Roaming/Autodesk/Neutron Platform/Options";
     "$HOME/.config/fusion360/local-config" = "drive_c/users/$USER/AppData/Local/Autodesk/Neutron Platform/Options";
   };
-
-  winAppRun = ''
-    exec wine64 "$WINEPREFIX/drive_c/Program Files/Autodesk/webdeploy/production/"*.exe "$ARGS"
-  '';
 
   desktopItems = [
     (makeDesktopItem {
@@ -152,8 +144,6 @@ mkWindowsApp rec {
     description = "Integrated CAD, CAM, and PCB design software";
     homepage = "https://www.autodesk.com/products/fusion-360/";
     license = licenses.unfree;
-    sourceProvenance = with sourceTypes; [binaryNativeCode];
     platforms = ["x86_64-linux"];
-    maintainers = with maintainers; [];
   };
 }
