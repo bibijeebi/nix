0cdd21dee7dae5dde1b824e1af63c2657078a182
bibijeebi <bennyforeman1@gmail.com>
2024-10-24 15:03:24 -0400

    chore: Refactor buildChromeExtension function and add Alt text downloader web extension

--- packages/web-extensions/altdown/default.nix
+++ packages/web-extensions/altdown/default.nix
@@ -1,15 +1,33 @@
+# packages/web-extensions/altdown/default.nix
 {
   lib,
   internal,
+  nodejs,
 }:
 internal.webext-utils.buildChromeExtension {
   pname = "altdown";
   version = "1.0.0";
   src = ./.;
 
+  # Optional: Override manifest values
+  manifestOverrides = {
+    description = "Enhanced alt text downloader extension";
+    permissions = ["activeTab" "downloads"];
+  };
+
+  # Optional: Add build-time dependencies
+  buildInputs = [nodejs];
+
+  # Optional: Add post-patch modifications
+  postPatch = ''
+    # Example: Modify some source files if needed
+    sed -i 's/DEBUG = true/DEBUG = false/' images.js
+  '';
+
   meta = with lib; {
     description = "Alt text downloader extension";
     license = licenses.mit;
     maintainers = with maintainers; [bibi];
+    platforms = platforms.all;
   };
 }
