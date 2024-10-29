3c3897ad3ffb954b1a7d6bf3634d36adc441c106
bibijeebi <bennyforeman1@gmail.com>
2024-10-29 00:18:02 -0400

    chore: update buildarr default nix config

--- snowfall/packages/buildarr/default.nix
+++ snowfall/packages/buildarr/default.nix
@@ -7,14 +7,16 @@
 }: let
   python = python311;
 
-  packages = rec {
-    prowlarr-py = callPackage ./prowlarr-py.nix {inherit lib python fetchPypi;};
-    radarr-py = callPackage ./radarr-py.nix {inherit lib python fetchPypi;};
-    buildarr-radarr = callPackage ./buildarr-radarr.nix {inherit lib python fetchPypi radarr-py;};
-    buildarr-sonarr = callPackage ./buildarr-sonarr.nix {inherit lib python fetchPypi;};
-    buildarr-prowlarr = callPackage ./buildarr-prowlarr.nix {inherit lib python fetchPypi prowlarr-py buildarr-radarr buildarr-sonarr;};
-    buildarr-jellyseerr = callPackage ./buildarr-jellyseerr.nix {inherit lib python fetchPypi buildarr-radarr buildarr-sonarr;};
-    buildarr = callPackage ./buildarr.nix {inherit lib python fetchPypi;};
-  };
+  prowlarr-py = callPackage ./prowlarr-py.nix {inherit lib python fetchPypi;};
+  radarr-py = callPackage ./radarr-py.nix {inherit lib python fetchPypi;};
+  buildarr-radarr = callPackage ./buildarr-radarr.nix {inherit lib python fetchPypi radarr-py;};
+  buildarr-sonarr = callPackage ./buildarr-sonarr.nix {inherit lib python fetchPypi;};
+  buildarr-prowlarr = callPackage ./buildarr-prowlarr.nix {inherit lib python fetchPypi prowlarr-py buildarr-radarr buildarr-sonarr;};
+  buildarr-jellyseerr = callPackage ./buildarr-jellyseerr.nix {inherit lib python fetchPypi buildarr-radarr buildarr-sonarr;};
+
+  buildarr = let
+    python = python.withPackages (ps: [buildarr-radarr buildarr-sonarr buildarr-prowlarr buildarr-jellyseerr]);
+  in
+    callPackage ./buildarr.nix {inherit lib python fetchPypi;};
 in
-  packages.buildarr
+  buildarr
