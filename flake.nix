deda89a17241c6fb3c06c97a11cfd54b54f3a699
bibijeebi <bennyforeman1@gmail.com>
2024-10-31 16:29:33 -0400

    refactor: restructure project directories for better organization

--- flake.nix
+++ flake.nix
@@ -1,43 +1,74 @@
 {
   inputs = {
+    # Core
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    snowfall-flake = {
-      url = "github:snowfallorg/flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    # System Management
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    nixarr.url = "git+file:///home/bibi/nixarr?shallow=1";
+    # Development
+    devshell.url = "github:numtide/devshell";
+    ez-configs.url = "github:ehllie/ez-configs";
 
-    erosanix.url = "github:emmanuelrosa/erosanix";
-
+    # Audio
     musnix.url = "github:musnix/musnix";
+
+    # Local
+    nixarr.url = "git+file:///home/bibi/nixarr?shallow=1";
   };
 
-  outputs = inputs:
-    inputs.snowfall-lib.mkFlake {
-      inherit inputs;
-      src = ./.;
-      
-      channels-config.allowUnfree = true;
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake {
+      imports = with inputs; [ ez-configs.flakeModule ];
 
-      systems.modules.nixos = with inputs; [
-        nixarr.nixosModules.default
-        musnix.nixosModules.default
-      ];
+      systems = [ "x86_64-linux" ];
 
-      overlays = with inputs; [
-        snowfall-flake.overlays.default
-      ];
+      ezConfigs = {
+        root = ./.;
+        globalArgs = { inherit inputs; };
+      };
+
+      perSystem = { pkgs, lib, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = with inputs; [ sops-nix.overlays.default ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = lib.attrValues {
+            inherit (pkgs)
+              cachix
+              deadnix
+              deploy-rs
+              manix
+              nix-diff
+              nix-direnv
+              nix-index
+              nix-info
+              nix-init
+              nix-melt
+              nix-output-monitor
+              nix-prefetch
+              nix-prefetch-github
+              nix-search-cli
+              nixd
+              nixfmt-rfc-style
+              nixos-generators
+              nixos-shell
+              nixtract
+              optinix
+              statix
+              ;
+          };
+        };
+      };
     };
 }
