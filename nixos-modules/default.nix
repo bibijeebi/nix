a98cdb57c56a3b554db7f759348916911f53e2ca
bibijeebi <bennyforeman1@gmail.com>
2024-11-01 11:38:27 -0400

    refactor: reorganize overlay and package structure

--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -1,51 +1,37 @@
+{ config, inputs, modulesPath, pkgs, ... }:
 {
-  config,
-  inputs,
-  lib,
-  modulesPath,
-  pkgs,
-  ...
-}:
-{
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports = [ 
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  nixpkgs.config.allowUnfree = true;
-
-  swapDevices = [ ]; # No swap configured
-
-  networking.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  environment = {
-    binsh = "${pkgs.dash}/bin/dash";
-    systemPackages = builtins.attrValues {
-      inherit (pkgs)
-        bash
-        coreutils
-        home-manager
-        pulseaudio
-        usbutils
-        ;
+  # System Configuration
+  system = {
+    stateVersion = "24.11";
+    autoUpgrade = {
+      enable = true;
+      allowReboot = true;
+      channel = "https://nixos.org/channels/nixos-unstable";
     };
   };
 
-  users.users.bibi = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "docker"
-      "video"
-      "audio"
-      "input"
-    ];
-    shell = pkgs.fish;
-    hashedPassword = "$6$qUiGCyo2rV6J.F2n$LWRdYGXUC.9trlQHWFjFJPBsd.nAkktcwlJNWZAtsZIyRt02AnO713q32hSJ0QxPWYzban3ekl64r6ny.XgHT/";
+  # Boot Configuration
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
+    efi.canTouchEfiVariables = true;
+    timeout = 1;
   };
 
+  # Hardware Configuration
+  hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+  swapDevices = [ ];
+
+  # Network Configuration
+  networking.useDHCP = true;
+
+  # Nix Configuration
   nix = {
     nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
     registry.nixpkgs.flake = inputs.nixpkgs;
@@ -53,10 +39,9 @@
     settings = {
       auto-optimise-store = true;
       trusted-users = [ "@wheel" ];
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
+      experimental-features = [ "nix-command" "flakes" ];
+      
+      # Cache Configuration
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
@@ -69,6 +54,7 @@
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
       ];
+      
       keep-outputs = true;
       keep-derivations = true;
     };
@@ -80,74 +66,86 @@
     };
   };
 
+  # Package Configuration
+  nixpkgs = {
+    config.allowUnfree = true;
+    hostPlatform = "x86_64-linux";
+  };
+
+  # Environment Configuration
+  environment = {
+    binsh = "${pkgs.dash}/bin/dash";
+    systemPackages = with pkgs; [
+      # Development Tools
+      alejandra deadnix direnv git gh home-manager nix-direnv
+      nix-output-monitor nixfmt-classic
+
+      # System Tools
+      coreutils htop parallel tmux tree usbutils
+
+      # Shell Tools
+      bash fish zsh bc curl eza fd jq ripgrep zoxide
+
+      # File Management
+      fdupes file p7zip unzip zip
+
+      # Media Tools
+      ffmpeg ffmpegthumbnailer imagemagick imv mpv
+      
+      # Document Tools
+      glow pandoc poppler_utils
+      
+      # Network Tools
+      nmap openvpn wget
+      
+      # Other Utilities
+      inotify-tools kitty manix pamixer pavucontrol pup
+      sqlite taskwarrior vim wl-clipboard yq
+    ];
+  };
+
+  # Font Configuration
   fonts.packages = [
     (pkgs.nerdfonts.override {
       fonts = [
-        "BigBlueTerminal"
-        "CascadiaCode"
-        "CascadiaMono"
-        "ComicShannsMono"
-        "DaddyTimeMono"
-        "FantasqueSansMono"
-        "FiraCode"
-        "FiraMono"
-        "Gohu"
-        "HeavyData"
-        "Hermit"
-        "Meslo"
-        "OpenDyslexic"
-        "ProggyClean"
-        "ShareTechMono"
-        "SourceCodePro"
-        "Terminus"
-        "Ubuntu"
-        "UbuntuMono"
+        "BigBlueTerminal" "CascadiaCode" "CascadiaMono"
+        "ComicShannsMono" "DaddyTimeMono" "FantasqueSansMono"
+        "FiraCode" "FiraMono" "Gohu" "HeavyData" "Hermit"
+        "Meslo" "OpenDyslexic" "ProggyClean" "ShareTechMono"
+        "SourceCodePro" "Terminus" "Ubuntu" "UbuntuMono"
       ];
     })
   ];
 
+  # Security Configuration
   security = {
     rtkit.enable = true;
-
     sudo = {
       enable = true;
       wheelNeedsPassword = true;
-      extraRules = [
-        {
-          groups = [ "wheel" ];
-          commands =
-            builtins.map
-              (command: {
-                inherit command;
-                options = [ "NOPASSWD" ];
-              })
-              [
-                "${pkgs.systemd}/bin/shutdown"
-                "${pkgs.systemd}/bin/reboot"
-              ];
-        }
-      ];
+      extraRules = [{
+        groups = [ "wheel" ];
+        commands = builtins.map
+          (command: {
+            inherit command;
+            options = [ "NOPASSWD" ];
+          })
+          [
+            "${pkgs.systemd}/bin/shutdown"
+            "${pkgs.systemd}/bin/reboot"
+          ];
+      }];
     };
   };
 
-  system = {
-    stateVersion = "24.11";
-    autoUpgrade = {
-      enable = true;
-      allowReboot = true;
-      channel = "https://nixos.org/channels/nixos-unstable";
-    };
+  # User Configuration
+  users.users.bibi = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel" "networkmanager" "video"
+      "audio" "input" "docker" "libvirtd"
+    ];
+    shell = pkgs.fish;
+    hashedPassword = "$6$qUiGCyo2rV6J.F2n$LWRdYGXUC.9trlQHWFjFJPBsd.nAkktcwlJNWZAtsZIyRt02AnO713q32hSJ0QxPWYzban3ekl64r6ny.XgHT/";
   };
-
-  boot = {
-    loader = {
-      systemd-boot = {
-        enable = true;
-        configurationLimit = 5;
-      };
-      efi.canTouchEfiVariables = true;
-      timeout = 0;
-    };
-  };
-
 }
